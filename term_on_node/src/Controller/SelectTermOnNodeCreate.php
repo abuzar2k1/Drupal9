2951113515d8d002a2956d721133d8f90afc9945
abuzar2k1 <abuzar2k1@gmail.com>
2021-10-14 00:07:48 +0530

    term on node updates

--- term_on_node/src/Controller/SelectTermOnNodeCreate.php
+++ term_on_node/src/Controller/SelectTermOnNodeCreate.php
@@ -3,10 +3,15 @@ namespace Drupal\term_on_node\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use \Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use \Drupal\file\Entity\File;
 
 class SelectTermOnNodeCreate extends ControllerBase{
 
     public function createNode() {
+
+        // Create file object from remote URL.
+        /*$data = file_get_contents('https://www.drupal.org/files/druplicon.small_.png');
+        $file = file_save_data($data, 'public://druplicon.png', FILE_EXISTS_REPLACE);*/
 
         $node = Node::create(array(
             'type' => 'new_content_type',
@@ -16,6 +21,13 @@ class SelectTermOnNodeCreate extends ControllerBase{
             //'created' => $data[$dateIndex],
             //'field_autor' => $data[$autorIndex],
             //'field_teaser_text' => $data[$shortIndex],
+
+            /*'field_image' => [
+                'target_id' => $file->id(),
+                'alt' => 'Hello world',
+                'title' => 'Goodbye world'
+            ],*/
+
             'field_selectfruit'  => [   // ATTACHING TERMS WITH NODE
               ['target_id' => 2], ['target_id' => 3]
             ]
