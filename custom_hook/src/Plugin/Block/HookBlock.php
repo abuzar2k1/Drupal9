b0ae78d9abbc63638145bc094bed7c7d331ba1ab
abuzar2k1 <abuzar2k1@gmail.com>
2021-11-04 17:50:48 +0530

    added custom hook module

--- custom_hook/src/Plugin/Block/HookBlock.php
+++ custom_hook/src/Plugin/Block/HookBlock.php
@@ -29,10 +29,10 @@ class HookBlock extends BlockBase implements ContainerFactoryPluginInterface {
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition){
 
     return new static(
-      $configuration,
-      $plugin_id, 
-      $plugin_definition,
-      $container->get('module_handler')
+        $configuration,
+        $plugin_id, 
+        $plugin_definition,
+        $container->get('module_handler')
     );
   }
 
