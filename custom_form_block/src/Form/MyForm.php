a4eff28cb7b8ff8bfe4d305f2c9bff8485db0734
abuzar2k1 <abuzar2k1@gmail.com>
2021-12-29 17:22:13 +0530

    added custom form block module

--- custom_form_block/src/Form/MyForm.php
+++ custom_form_block/src/Form/MyForm.php
@@ -4,6 +4,7 @@ namespace Drupal\custom_form_block\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
 
 /**
  * Class MyForm.
@@ -48,10 +49,39 @@ class MyForm extends FormBase {
       '#description' => $this->t('Enter your email'),
       '#weight' => '0',
     ];
+
+    $form['file'] = [
+      '#type' => 'managed_file',
+      '#title' => t('File'),
+      '#upload_location' => 'public://file',
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png'],
+      ],
+      //'#default_value' => array(2)  // "2" here is the file id
+    ];
+
+    // multiple file upload
+    $form['upload']['image_dir'] = [
+
+      '#type'                 => 'managed_file',
+      '#upload_location'      => 'public://',
+      '#multiple'             => TRUE,
+      '#description'          => t('Allowed extensions: gif png jpg jpeg'),
+      '#upload_validators'    => [
+        'file_validate_is_image'      => array(),
+        'file_validate_extensions'    => array('gif png jpg jpeg'),
+        'file_validate_size'          => array(25600000)
+      ],
+      '#title'                => t('Upload an image file for this slide')
+
+    ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
     ];
+
+    
 
     return $form;
   }
@@ -67,6 +97,33 @@ class MyForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $fid = $form_state->getValue(['file', 0]);
+    if (!empty($fid)) {
+
+      $file = File::load($fid);
+      $file->setPermanent();
+      $file->save();
+
+      $filename = $file->getFilename();
+    }
+
+    // multiple file upload
+    $file_data = $form_state->getValue(['upload' => 'image_dir']);
+    //$file = \Drupal\file\Entity\File::load( $file_data[0] );
+    $file = File::load( $file_data[0] );
+    $file_name = $file->getFilename();
+    $file->setPermanent();
+    $file->save();
+
+    //Here we will get file name which we can save it in our table.
+    //$file_data will be an array which contains file id. If multiple files are selected , 
+    //then multiple values will be there in this array.
+
+    //print_r($file_data);
+    //die;
+    //output Array ( [0] => 8 [1] => 9 [2] => 10 )
+
     // Display result.
     foreach ($form_state->getValues() as $key => $value) {
       \Drupal::messenger()->addMessage($key . ': ' . $value);
